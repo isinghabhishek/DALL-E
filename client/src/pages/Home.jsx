9d915472df4fc5c1526857ea19fb3e41c697005f
Abhishek Singh <singhabhishek3006.abhi@gmail.com>
2023-01-21 20:02:05 +0530

    url update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             setLoading(true);
 
             try {
-                const response = await fetch('https://respect.onrender.com', {
+                const response = await fetch('https://respect.onrender.com/api/v1/post', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
